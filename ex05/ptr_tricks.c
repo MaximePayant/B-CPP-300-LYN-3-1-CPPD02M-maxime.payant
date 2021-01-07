2bcd7391293b0ac8e1e05806fa92168fae7080ee
Maxime Payant <maxime@pop-os.localdomain>
2021-01-07 14:12:21 +0100

    Back to the future

---
+++ ex05/ptr_tricks.c
@@ -0,0 +1,23 @@
+/*
+** ShipWreck engine, 2020
+** ptr_tricks.c
+*/
+
+#include "ptr_tricks.h"
+
+int get_array_nb_elem(const int *ptr1, const int *ptr2)
+{
+    int len = 0;
+
+    while (ptr1 + len != ptr2)
+        len += 1;
+    return (len);
+}
+
+whatever_t *get_struct_ptr(const int *member_ptr)
+{
+    const whatever_t test;
+    int len = get_array_nb_elem((const int *)(&test), &test.member);
+
+    return ((whatever_t *)(member_ptr - len));
+}
